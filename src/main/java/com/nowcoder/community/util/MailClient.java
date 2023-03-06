5ea09ffc0f6a82003651bad46a8560c08e9e556d
Zhikai Xu <3180111125@zju.edu.cn>
2023-03-06 17:57:27 +0800

    Test commit

---
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -0,0 +1,40 @@
+package com.nowcoder.community.util;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailClient {
+    // 之后需要打印日志
+    private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
+
+    // 需要spring管理的邮件组件的依赖注入
+    @Autowired
+    private JavaMailSender mailSender;
+
+    // 发送人，将username注入当前的bean以便之后复用
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendMail(String to, String subject, String content) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            logger.error("发送邮件失败: " + e.getMessage());
+        }
+    }
+
+}
