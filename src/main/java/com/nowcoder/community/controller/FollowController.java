59f83520c23b6f61e31d7b99b8cf056346d97e57
Zhikai Xu <3180111125@zju.edu.cn>
2023-04-11 16:09:14 +0800

    完成关注、取消关注功能

---
+++ src/main/java/com/nowcoder/community/controller/FollowController.java
@@ -0,0 +1,45 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.FollowService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FollowController {
+
+    @Autowired
+    private FollowService followService;
+
+    @Autowired
+    private HostHolder hostHolder; // 除了entityType、entityId两个参数，还需要获取用户来得到userId这个参数
+
+    // 异步请求
+    @RequestMapping(path = "/follow", method = RequestMethod.POST)
+    @ResponseBody
+    public String follow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        // 可以用拦截器来拦截用户是否已经登录
+
+        followService.follow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已关注!");
+    }
+
+    @RequestMapping(path = "/unfollow", method = RequestMethod.POST)
+    @ResponseBody
+    public String unfollow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.unfollow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已取消关注!");
+    }
+
+}
--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.FollowService;
 import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import io.micrometer.common.util.StringUtils;
@@ -26,7 +28,7 @@ import java.io.OutputStream;
 
 @Controller
 @RequestMapping(path = "/user")
-public class UserController {
+public class UserController implements CommunityConstant {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -47,6 +49,9 @@ public class UserController {
 
     @Autowired
     private LikeService likeService;
+
+    @Autowired
+    private FollowService followService;
 
     @LoginRequired // 自定义注解：登录后才能访问
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
@@ -144,6 +149,19 @@ public class UserController {
         // 点赞数量
         int likeCount = likeService.findUserLikeCount(userId);
         model.addAttribute("likeCount", likeCount);
+
+        // 关注数量
+        long followeeCount = followService.findFolloweeCount(userId, ENTITY_TYPE_USER);
+        model.addAttribute("followeeCount", followeeCount);
+        // 粉丝数量
+        long followerCount = followService.findFollowerCount(ENTITY_TYPE_USER, userId);
+        model.addAttribute("followerCount", followerCount);
+        // 当前登录用户是否已经关注改用户
+        boolean hasFollowed = false;
+        if (hostHolder.getUser() != null) {
+            hasFollowed = followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+        }
+        model.addAttribute("hasFollowed", hasFollowed);
 
         return "/site/profile";
     }
