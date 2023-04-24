89679740cb88d56889e15e8cb23b50f543ced5d6
Zhikai Xu <3180111125@zju.edu.cn>
2023-04-24 11:16:53 +0800

    完成Spring整合Elasticsearch

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -9,12 +9,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 public class CommunityApplication {
 
-//	@PostConstruct
-//	public void init() {
-//		// 解决netty启动冲突问题
-//		// see Netty4Utils.setAvailableProcessors()
-//		System.setProperty("es.set.netty.runtime.available.processors", "false");
-//	}
+	@PostConstruct
+	public void init() {
+		// 解决netty启动冲突问题
+		// see Netty4Utils.setAvailableProcessors()
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
